417bf1b0dca09ce8e6dc2e011be49e7995232c31
lasags <lasags@naver.com>
2022-07-12 16:20:41 +0900

    pt1.js

---
+++ pt1.ts
@@ -0,0 +1,24 @@
+let a = 'hello';
+a = 'bye';
+// a = 1;
+
+let b = true;
+b = false;
+// b = 1;
+
+let c: number = 3;
+// c = "three";
+
+let d: string = 'today';
+// d = true;
+
+let e = [1, 2, 3];
+// e.push("5");
+e.push(5);
+
+const player = {
+  name: 'yoon',
+};
+player.name = 'JungYoon';
+
+console.log(player, e);
