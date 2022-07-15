0c4bc77cd58c0b769ec30b3ae122c05686303675
lasags <lasags@naver.com>
2022-07-16 01:46:09 +0900

    arrow_function, overloading

--- 3-Functions/3-1.ts
+++ 3-Functions/3-1.ts
@@ -1,7 +1,7 @@
 // 타입스크립트 함수 선언식
-function add(a: number, b: number){
-  return a + b
-}
+// function add(a: number, b: number){
+//   return a + b
+// }
 
 // 타입스크립트 함수 표현식
 
@@ -15,3 +15,27 @@ let plus = function(a: number, b: number){
 type Minus = (a: number, b:number) => number;
 const minus:Minus = (a,b) => a + b
 
+// type Add = {
+//   (a:number, b:number) :number
+//   (a:number, b:string) :number
+// }
+
+// const add: Add = (a, b) => {
+//   if(typeof b === "string") return a
+//   return a + b
+// }
+
+/// Overloading : 여러 call signatures가 있는 함수
+
+type Add = {
+  (a:number, b:number) : number
+  (a:number, b:number, c:number) : number,
+}
+
+const add:Add = (a, b, c?:number) => {
+  if(c) return a + b + c
+  return a + b
+}
+
+console.log(add(1,5))
+console.log(add(1,5,4))
