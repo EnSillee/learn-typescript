ff4f228dc0e790292b01daa063c7b85adbb2df26
lasags <lasags@naver.com>
2022-07-15 23:56:29 +0900

    add 3-1.ts

---
+++ 3-1.ts
@@ -0,0 +1,14 @@
+// 타입스크립트 함수 tjsdjs식
+function add(a: number, b: number){
+  return a + b
+}
+
+// 타입스크립트 함수 표현식
+
+let plus = function(a: number, b: number){
+  return a + b
+}
+
+// 타입스크립트 화살표함수
+let minus = (a: number, b: number) => (a - b)
+
