2831764a53183ae632de417b39a4489ff5ce03af
lasags <lasags@naver.com>
2022-07-16 00:35:13 +0900

    add chapter folder

--- 3-Functions/3-1.ts
+++ 3-Functions/3-1.ts
@@ -1,4 +1,4 @@
-// 타입스크립트 함수 tjsdjs식
+// 타입스크립트 함수 선언식
 function add(a: number, b: number){
   return a + b
 }
@@ -10,5 +10,8 @@ let plus = function(a: number, b: number){
 }
 
 // 타입스크립트 화살표함수
-let minus = (a: number, b: number) => (a - b)
+// let minus = (a: number, b: number) => (a - b)
+
+type Minus = (a: number, b:number) => number;
+const minus:Minus = (a,b) => a + b
 
